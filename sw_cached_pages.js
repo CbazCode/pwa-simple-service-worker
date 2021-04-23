c4dd373a4047102f493c688bd6feb5dc2c9c8be1
Sebastian Asis <sasisromero10@gmail.com>
2021-04-23 13:27:31 -0500

    :fire: NEW: simple service worker

---
+++ sw_cached_pages.js
@@ -0,0 +1,48 @@
+const cacheName = 'v1';
+
+const cacheAssets = [
+  'index.html',
+  'about.html',
+  '/css/style.css',
+  '/js/main.js'
+];
+
+// Call Install Event
+self.addEventListener('install', e => {
+  console.log('Service Worker: Installed');
+
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log('Service Worker: Caching Files');
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Call Activate Event
+self.addEventListener('activate', e => {
+  console.log('Service Worker: Activated');
+  // Remove unwanted caches (old cach, quita las caches si cambiamos el nombre de la version)
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log('Service Worker: Clearing Old Cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Call Fetch Event
+self.addEventListener('fetch', e => {
+  console.log('Service Worker: Fetching');
+  //catch.match : carga todo lo que esta en la cache y lo coloca en el e.request
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
